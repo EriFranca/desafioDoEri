8d459b33c615c800c1b87450c0309dadf62eb424
Eri França <ecfs1988@hotmail.com>
2023-11-09 20:24:13 -0300

    desafio heroi xp

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
-let nomeHeroi = "Eri";
-let xpHeroi = 1000;
+const input = require('readline-sync');//Para entrada de dados
 
-console.log(xpHeroi)
+let nomeHeroi = "";
+let xpHeroi = 0;
+let nivel = "";
+
+//repetição
+while (true){
+    //Operador de atribuição via teclado
+    nomeHeroi = input.question("O seu nome? ");
+    xp = input.questionInt("Qual sua experiência? ");
+
+    if (xp < 1000) {
+        nivel = "Ferro";
+    } else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+    } else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina";
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+    } else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+    } else if (xp >= 10001) {
+        nivel = "Radiante";
+    }
+
+    //possiveis saidas
+    if (nomeHeroi !== "" && xp !== 0)
+        console.log(`O Herói: ${nomeHeroi}, está no nível: ${nivel}.`);
+    else
+        console.log(`Você precisa informar o nome do herói e seu xp para verificar o nível.`);
+
+
+    let verificarOutroNivel = input.question("Deseja verificar o nivel de mais algum heroi [s/n]: ");
+
+    // Sai do loop quando o usuário digitar n
+    if (verificarOutroNivel.toLowerCase() === 'n')
+        break;
+}
